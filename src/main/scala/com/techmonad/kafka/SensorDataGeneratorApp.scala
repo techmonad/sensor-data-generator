82809d802b1d7097a59962799fdac914a8da28d6
Satendra Kumar <satendrakumar06@gmail.com>
2018-07-15 14:56:45 +0530

    Update sleeping time

--- src/main/scala/com/techmonad/kafka/SensorDataGeneratorApp.scala
+++ src/main/scala/com/techmonad/kafka/SensorDataGeneratorApp.scala
@@ -9,11 +9,11 @@ object SensorDataGeneratorApp extends App {
   val rand = new Random
 
   while (true) {
-    val id = rand.nextInt(100)
+   // val id = rand.nextInt(100)
     val temperature: Float = rand.nextInt(120)
     val time = System.currentTimeMillis()
-    producer.send("data_queue", s"$id,$temperature,$time")
-    Thread.sleep(100)
+    producer.send("data_queue3", s"$time,$temperature,$time")
+    Thread.sleep(10)
   }
 
 }
